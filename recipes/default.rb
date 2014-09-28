cbffb7ac7b25c8d46cff4d68ff271c33d3adf04a
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-09-28 22:40:57 +0900

    Add Chef DK

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,13 @@ jenkins_plugin "git"
 jenkins_plugin "docker-build-publish"
 
 include_recipe "docker"
+
+chefdk_file_path = "#{Chef::Config[:file_cache_path]}/chefdk_0.2.2-1_amd64.deb"
+remote_file chefdk_file_path do
+  source "https://packagecloud.io/chef/stable/download?distro=precise&filename=chefdk_0.2.2-1_amd64.deb"
+end
+
+dpkg_package "chefdk" do
+  source chefdk_file_path
+  action :install
+end
