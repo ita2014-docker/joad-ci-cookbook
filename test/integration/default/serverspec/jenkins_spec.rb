ebec377a032c332b16db0442202190b1498c19f1
Takahiro Yano <Yano_Takahiro@ogis-ri.co.jp>
2014-10-08 19:00:13 +0900

    Add testing whether command installed

--- test/integration/default/serverspec/jenkins_spec.rb
+++ test/integration/default/serverspec/jenkins_spec.rb
@@ -6,3 +6,9 @@ describe service('jenkins') do
   it { should be_enabled }
   it { should be_running }
 end
+
+%w{docker chef berks foodcritic kitchen knife}.each do |cmd|
+  describe command("which #{cmd}") do
+    its(:exit_status) { should eq 0 }
+  end
+end
